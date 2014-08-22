99b87e66f978eccf68c8a3d2a5ebaa602b2242f9
yamapon <yasuakiya@gmail.com>
2014-08-22 21:52:01 +0900

    更新したのでコミット

--- Nodeset.R
+++ Nodeset.R
@@ -1,21 +1,42 @@
-library(SQLite)
 library(XML)
 
 accountsTitle <- read.csv('C:/Users/kikuchi/Downloads/1f_copy.csv',header=TRUE)
 workDir <- "c:/Users/kikuchi/Downloads/xbrl/work/XBRLData/Attachment/"
-fileName <- "0101010-acbs01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
+fileNameBS <- "0101010-acbs01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
+fileNamePL <- "0102010-acpl01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
+fileNameCF <- "0104010-accf01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
 
-fileAttrs <- strsplit(fileName,"-")
+fileAttrs <- strsplit(fileNameCF,"-")
 
-fullFileName <- paste(workDir,fileName,sep="")
+fullFileName <- paste(workDir,fileNamePL,sep="")
 doc <- xmlInternalTreeParse(fullFileName,useInternalNodes = TRUE)
 xmlData <- xmlRoot(doc)
 
-for( i in 1:length(accountsTitle$element)){
+accountsDf <- data.frame()
+for( i in seq_along(accountsTitle$element)){
   item <- paste(accountsTitle$prefix[i],':',accountsTitle$element[i],sep="")  
-  context <- paste("//*[@name='",item,"']",sep="")
-  attrs <- xpathSApply(xmlData,context,xmlAttrs)
-  if( lenght(attrs) > 0 ){
-    value <- xpathSApply(result,xmlValue)
+
+  context <- paste("//*[@name='",item,"' and @contextRef='CurrentYearDuration' and not(@xsi:nil) ]",sep="")
+  
+  column <- xpathSApply(xmlData,context,xmlAttrs)
+  
+  if( length(column) > 0 ){
+    value <- xpathSApply(xmlData,context,xmlValue)
+    if( length(value) > 0 ){
+      columnValue <- c()
+      for( j in seq_along(column)){
+        if(is.na(match("footnoteRefs",names(strsplit(column[j,],"\n"))))){
+          columnValue <- append(columnValue,strsplit(column[j,],"\n"))
+        }
+      }
+      if( is.na(match("sign", names(columnValue))) ){
+        columnValue <- append(columnValue,list(sign=c('')))        
+      }
+      #columnValue <- append(columnValue,list(value=c(as.numeric(sub(",","",value)))))
+      columnValue <- append(columnValue,list(value=c(value)))
+      accountsDf <- rbind(accountsDf,as.data.frame.list(columnValue,stringsAsFactors = FALSE))
+      print(columnValue)         
+    }
+  
   }
 }
