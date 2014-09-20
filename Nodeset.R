346cc708876dfa8a5f8f328571f2d7a5954c2265
yamapon <yasuakiya@gmail.com>
2014-09-20 21:27:40 +0900

    mac用に修正

--- Nodeset.R
+++ Nodeset.R
@@ -1,34 +1,41 @@
 library(XML)
 
-accountsTitle <- read.csv('C:/Users/kikuchi/Downloads/1f_copy.csv',header=TRUE)
-workDir <- "c:/Users/kikuchi/Downloads/xbrl/work/XBRLData/Attachment/"
-fileNameBS <- "0101010-acbs01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
-fileNamePL <- "0102010-acpl01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
-fileNameCF <- "0104010-accf01-tse-acedjpfr-67560-2014-03-31-01-2014-04-24-ixbrl.htm"
+accountsTitle <- read.csv('/Users/yasuaki/Documents/XBRLData/1f_copy.csv',header=TRUE,fileEncoding = 'cp932')
 
 instanceFileParser <- function(fullFileName){
   
   fileAttrs <- strsplit(fileName,"-")
   
-  column <- xpathSApply(xmlData,context,xmlAttrs)
+  #fullFileName <- paste(workDir,fileName,sep="")
+  doc <- xmlInternalTreeParse(fullFileName,useInternalNodes = TRUE)
+  xmlData <- xmlRoot(doc)
   
-  if( length(column) > 0 ){
-    value <- xpathSApply(xmlData,context,xmlValue)
-    if( length(value) > 0 ){
-      columnValue <- c()
-      for( j in seq_along(column)){
-        if(is.na(match("footnoteRefs",names(strsplit(column[j,],"\n"))))){
-          columnValue <- append(columnValue,strsplit(column[j,],"\n"))
+  accountsDf <- data.frame()
+  for( i in seq_along(accountsTitle$element)){
+    item <- paste(accountsTitle$prefix[i],':',accountsTitle$element[i],sep="")  
+  
+    context <- paste("//*[@name='",item,"' and @contextRef='CurrentYearDuration' and not(@xsi:nil) ]",sep="")
+    
+    column <- xpathSApply(xmlData,context,xmlAttrs)
+    
+    if( length(column) > 0 ){
+      value <- xpathSApply(xmlData,context,xmlValue)
+      if( length(value) > 0 ){
+        columnValue <- c()
+        for( j in seq_along(column)){
+          if(is.na(match("footnoteRefs",names(strsplit(column[j,],"\n"))))){
+            columnValue <- append(columnValue,strsplit(column[j,],"\n"))
+          }
         }
+        if( is.na(match("sign", names(columnValue))) ){
+          columnValue <- append(columnValue,list(sign=c('')))        
+        }
+        #columnValue <- append(columnValue,list(value=c(as.numeric(sub(",","",value)))))
+        columnValue <- append(columnValue,list(value=c(value)))
+        accountsDf <- rbind(accountsDf,as.data.frame.list(columnValue,stringsAsFactors = FALSE))
       }
-      if( is.na(match("sign", names(columnValue))) ){
-        columnValue <- append(columnValue,list(sign=c('')))        
-      }
-      #columnValue <- append(columnValue,list(value=c(as.numeric(sub(",","",value)))))
-      columnValue <- append(columnValue,list(value=c(value)))
-      accountsDf <- rbind(accountsDf,as.data.frame.list(columnValue,stringsAsFactors = FALSE))
-      print(columnValue)         
+    
     }
-  
   }
+  return(accountsDf)
 }
